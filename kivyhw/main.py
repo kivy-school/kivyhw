20cc9688f5900cff9923c1520392d8f312dbe377
AccelQuasarDragon <138998466+AccelQuasarDragon@users.noreply.github.com>
2024-05-02 10:01:57 -0700

    window on top + window title

--- kivyhw/main.py
+++ kivyhw/main.py
@@ -1,7 +1,16 @@
 from kivy.app import runTouchApp
 from kivy.lang import Builder
+from kivy.core.window import Window
+
+#this is to make the Kivy window always on top
+Window.always_on_top = True
+Window.set_title('Welcome to Kivy School!')
+
+#kv language setting the main widget to be a button
 kvString = '''
 Button:
     text: "Hello world!"
 '''
+
+#run the Kivy app
 runTouchApp(Builder.load_string(kvString))
